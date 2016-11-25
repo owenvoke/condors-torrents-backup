8f507dac2c9d82f19e4881afa8078929953e413e
PXgamer <owzie123@gmail.com>
2016-11-25 13:59:43 +0000

    Fixed issue #3

--- resources/GetCondors.php
+++ resources/GetCondors.php
@@ -6,7 +6,8 @@ for ($j = 1; $j < 18; $j++) {
     $data = grab($url);
     $full_list = json_decode(file_get_contents("./output.json"));
     preg_match_all('/<a href="\/download\/(.*?)\/.*?\.torrent" title="Download (.*?) torrent"><img src="\/\/images4et.com\/images\/icon_download3.gif" alt="Download" \/><\/a><a href="magnet:\?xt=urn:btih:(.*?)&amp;/', $data, $matches);
-    for ($i = 0; $i < count($matches[1]); $i++) {
+    $total = count($matches[1]);
+	for ($i = 0; $i < $total; $i++) {
         $full_list[] = (object)[
             "id" => $matches[1][$i],
             "title" => $matches[2][$i],
